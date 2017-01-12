8cdad0ccfd5c7d50508983148f86a7cb89f3be73
NguyenhoangDuy1123 <nghduy11011995@gmail.com>
2017-01-13 02:32:52 +0700

    Cài dặt song song phần Swap

--- student_func.cu
+++ student_func.cu
@@ -61,32 +61,85 @@ __global__ void histogram(unsigned int* in, unsigned int* hist, int n,unsigned i
 __global__ void scan(unsigned int *in,unsigned int *out, int n)
 {   
 	extern __shared__ int blkData[];
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	if (i < n)
-		blkData[threadIdx.x] = in[i];
-	__syncthreads();
 
+ 	if (threadIdx.x < n)
+ 		blkData[threadIdx.x] = in[threadIdx.x];
+ 	__syncthreads();
+ 
 	for (int stride = 1; stride < blockDim.x; stride *= 2)
-	{
-		int left_value;
-		if (threadIdx.x >= stride)
-			left_value = blkData[threadIdx.x - stride];
-		__syncthreads();
-		
-		if (threadIdx.x >= stride)
-			blkData[threadIdx.x] += left_value;
-		__syncthreads();
-	}
+ 	{
+ 		int left_value;
+ 		if (threadIdx.x >= stride)
+ 			left_value = blkData[threadIdx.x - stride];
+ 		__syncthreads();
+ 		
+ 		if (threadIdx.x >= stride)
+ 			blkData[threadIdx.x] += left_value;
+ 		__syncthreads();
+ 	}
+ 
+ 	if (threadIdx.x < n)
+ 		out[threadIdx.x] = blkData[threadIdx.x];
+}
 
+__global__ void excusive_scan(unsigned int *in,unsigned int *out, int n)
+{   
+	extern __shared__ int blkData[];
+
+ 	if (threadIdx.x < n)
+ 	{
+		out[threadIdx.x] -= in[threadIdx.x];
+	}
+}
+
+__global__ void scatter(unsigned int *in,unsigned int *in_pos, unsigned int *out, unsigned int *out_pos, unsigned int n, unsigned int *histScan, unsigned int mask, unsigned int current_bits, unsigned int *d_histBlock, unsigned int *d_histSave)
+{
+	extern __shared__ unsigned int scatter_blkData[];
+	
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	
 	if (i < n)
-		if(i == 0)
-			out[i] = 0;
-		else
-			out[i] = blkData[i - 1];
+	{
+		scatter_blkData[threadIdx.x] = in[i];
+		scatter_blkData[threadIdx.x + blockDim.x] = in_pos[i];
+	}
+	__syncthreads();
+	
+	unsigned int bin = (scatter_blkData[threadIdx.x] >> current_bits) & mask;
+	
+	unsigned int save = d_histSave[bin];
+	__syncthreads();
+	
+	unsigned int return_value = atomicSub(&d_histBlock[bin], 1);
+	__syncthreads();
+	
+	if (i < n)
+	{
+		out[histScan[bin] + (save - return_value)] = scatter_blkData[threadIdx.x];
+		out_pos[histScan[bin] + (save - return_value)] = scatter_blkData[threadIdx.x + blockDim.x];
+	}
+}
+
+__global__ void swap(unsigned int *in,unsigned int *in_pos, unsigned int *out, unsigned int *out_pos, unsigned int n)
+{
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	if (i < n)
+	{
+		unsigned int temp = in[i];
+		in[i] = out[i];
+		out[i] = temp;
+		
+		temp = in_pos[i];
+		in_pos[i] = out_pos[i];
+		out_pos[i] = temp;
+	}
 }
 
 const dim3 hist_blockSize(256);
 const dim3 scan_blockSize(256);
+const dim3 scatter_blockSize(256);
+const dim3 swap_blockSize(256);
 
 void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_inputPos,
@@ -103,43 +156,65 @@ void your_sort(unsigned int* const d_inputVals,
 		}
 	
 	dim3 hist_gridSize((numElems - 1)/(hist_blockSize.x) + 1);
+	dim3 scatter_gridSize((numElems - 1)/(scatter_blockSize.x) + 1);
+	dim3 swap_gridSize((numElems - 1)/(swap_blockSize.x) + 1);
+	
     unsigned int nBins = 1 << nBits;
     unsigned int *d_hist;
 	cudaMalloc(&d_hist, nBins * sizeof(unsigned int));
 	
-    unsigned int* histScan = new unsigned int[nBins];
+	unsigned int *d_histSave;
+	cudaMalloc(&d_histSave, nBins * sizeof(unsigned int));
+	
+	unsigned int *d_histBlock;
+	cudaMalloc(&d_histBlock, nBins * sizeof(unsigned int));
+	
 	unsigned int *d_histScan;
 	cudaMalloc(&d_histScan, nBins * sizeof(unsigned int));
-    
-    unsigned int* src = new unsigned int[numElems];
-    unsigned int* src_pos = new unsigned int[numElems];
-    
-    unsigned int* des = new unsigned int[numElems];
-    unsigned int* des_pos = new unsigned int[numElems];
-    
-    cudaMemcpy(src, d_inputVals, numElems * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(src_pos, d_inputPos, numElems * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-    
-	unsigned int *d_src;
-	cudaMalloc(&d_src, numElems * sizeof(unsigned int));
 	
 	unsigned int mask = (1 << nBits) - 1;
+	
+	
+	unsigned int *src;
+	src = new unsigned int[numElems];
+	
+	unsigned int *src_pos;
+	src_pos = new unsigned int[numElems];
+	
+	unsigned int *des;
+	des = new unsigned int[numElems];
+	
+	unsigned int *des_pos;
+	des_pos = new unsigned int[numElems];
+	
+	unsigned int *histScan;
+	histScan = new unsigned int[nBins];
+	
     for (unsigned int i = 0; i < sizeof(unsigned int)*8; i += nBits)
     {
-		cudaMemcpy(d_src, src, numElems * sizeof(unsigned int), cudaMemcpyHostToDevice);
 		//Histogram
 		cudaMemset(d_hist, 0, nBins * sizeof(unsigned int));
-        
-		histogram<<<hist_gridSize, hist_blockSize, nBins*sizeof(unsigned int)>>>(d_src, d_hist, numElems, nBins, mask, i);
 		
+		histogram<<<hist_gridSize, hist_blockSize, nBins*sizeof(unsigned int)>>>(d_inputVals, d_hist, numElems, nBins, mask, i);
 		cudaDeviceSynchronize();
-		 
+		
+		/*cudaMemcpy(d_histSave, d_hist, nBins * sizeof(unsigned int), cudaMemcpyDeviceToDevice);
+		cudaMemcpy(d_histBlock, d_hist, nBins * sizeof(unsigned int), cudaMemcpyDeviceToDevice);*/
+		
 		//Exclusive Scan
 		scan<<<1, scan_blockSize, scan_blockSize.x*sizeof(unsigned int)>>>(d_hist, d_histScan, nBins);
-		cudaMemcpy(histScan, d_histScan, nBins * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-		
+		excusive_scan<<<1, scan_blockSize, scan_blockSize.x*sizeof(unsigned int)>>>(d_hist, d_histScan, nBins);
 		cudaDeviceSynchronize();
-            
+		
+		/*//Scatter
+		scatter<<<scatter_gridSize, scatter_blockSize, 2*scatter_blockSize.x*sizeof(unsigned int)>>>(d_inputVals, d_inputPos, d_outputVals, d_outputPos, numElems, d_histScan, mask, i, d_histBlock, d_histSave);
+		cudaDeviceSynchronize();*/
+		
+		cudaMemcpy(src, d_inputVals, numElems * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(src_pos, d_inputPos, numElems * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(des, d_outputVals, numElems * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(des_pos, d_outputPos, numElems * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(histScan, d_histScan, nBins * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 		// Scatter
         for (unsigned int j = 0; j < numElems; j++)
         {
@@ -148,24 +223,20 @@ void your_sort(unsigned int* const d_inputVals,
 			des_pos[histScan[bin]] = src_pos[j];
             histScan[bin]++;
         }
-        
+		
+		cudaMemcpy(d_inputVals, src, numElems * sizeof(unsigned int), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_inputPos, src_pos, numElems * sizeof(unsigned int), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_outputVals, des, numElems * sizeof(unsigned int), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_outputPos, des_pos, numElems * sizeof(unsigned int), cudaMemcpyHostToDevice);
+		
 		//Swap
-        for (unsigned int j = 0; j < numElems; j++)
-        {
-            unsigned int temp = src[j];
-            src[j] = des[j];
-            des[j] = temp;
-			
-			temp = src_pos[j];
-            src_pos[j] = des_pos[j];
-            des_pos[j] = temp;
-        }
+		swap<<<swap_gridSize, swap_blockSize>>>(d_inputVals, d_inputPos, d_outputVals, d_outputPos, numElems);
+		cudaDeviceSynchronize();
     }
 	
-	cudaMemcpy(d_outputVals, src, numElems * sizeof(unsigned int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_outputPos, src_pos, numElems * sizeof(unsigned int), cudaMemcpyHostToDevice);
-    
-    delete[] histScan;
+	swap<<<swap_gridSize, swap_blockSize>>>(d_inputVals, d_inputPos, d_outputVals, d_outputPos, numElems);
+	
+	delete[] histScan;
     delete[] src;
     delete[] src_pos;
     delete[] des;
